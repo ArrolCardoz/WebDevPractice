b0e033b24a7a5396dec002be32122d6f9f8fa9c7
Arrol Cardoz <cardozarrol@gmail.com>
2025-08-26 12:42:41 -0600

    passing data

--- Nodejs/4.2 PassingData/index.js
+++ Nodejs/4.2 PassingData/index.js
@@ -1,14 +1,29 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { render } from "ejs";
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {});
+app.get("/", (req, res) => {
+  const data = {
+    firstName: req.body.fName,
+    lastName: req.body.lName,
+  };
+  res.render("index.ejs", data);
+});
 
-app.post("/submit", (req, res) => {});
+app.post("/submit", (req, res) => {
+  var firstName = req.body.fName;
+
+  const data = {
+    firstName: req.body.fName,
+    lastName: req.body.lName,
+  };
+  res.render("index.ejs", data);
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
