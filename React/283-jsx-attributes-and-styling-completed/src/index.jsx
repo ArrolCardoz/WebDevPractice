bc37c9637df393960beec73ce327f8d5ec207525
Arrol Cardoz <cardozarrol@gmail.com>
2026-05-04 00:04:58 -0600

    jsx styling

--- React/283-jsx-attributes-and-styling-completed/src/index.jsx
+++ React/283-jsx-attributes-and-styling-completed/src/index.jsx
@@ -5,26 +5,16 @@ const img = "https://picsum.photos/200";
 
 ReactDOM.render(
   <div>
-    <h1 className="heading">My Favourite Foods</h1>
-    <img alt="random" src={img + "?grayscale"} />
-
-    <img
-      className="food-img"
-      alt="bacon"
-      src="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/delish-190621-air-fryer-bacon-0035-landscape-pf-1567632709.jpg?crop=0.645xw:0.967xh;0.170xw,0.0204xh&resize=480:*"
-    />
-    <img
-      className="food-img"
-      alt="jamon"
-      src="https://images-na.ssl-images-amazon.com/images/I/71lNrnbMXsL._SL1200_.jpg"
-    />
-    <img
-      className="food-img"
-      alt="noodles"
-      src="https://www.errenskitchen.com/wp-content/uploads/2014/04/quick-and-easy-chinese-noodle-soup3-1.jpg"
-    />
+    <h1 className="heading" contentEditable="true" spellCheck="false">
+      My Favourite Foods
+    </h1>
+    <div>
+      <img className="fav-food-img" src={img + "?random=1"}></img>
+      <img className="fav-food-img" src={img + "?random=2"}></img>
+      <img className="fav-food-img" src={img + "?random=3"}></img>
+    </div>
   </div>,
-  document.getElementById("root")
+  document.getElementById("root"),
 );
 
 // If you're running this locally in VS Code use the commands:
